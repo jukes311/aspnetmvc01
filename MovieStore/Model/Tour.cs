2d3ca49db71cf5f2ee0e45c0c8868998e4f66992
Chris <jukes311@gmail.com>
2020-04-06 18:46:47 -0400

    Added Tour model.

---
+++ MovieStore/Model/Tour.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieStore.Model
+{
+    public class Tour
+    {
+        public string code { get; set; }
+        public DateTime tourDate { get; set; }
+        public string name { get; set; }
+        public string venue { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zip { get; set; }
+        public int capacity { get; set; }
+        public double price { get; set; }
+    }
+}
