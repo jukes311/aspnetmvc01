26a0741afc5fad7cbc0fe77114087205c73def1f
Chris <jukes311@gmail.com>
2020-04-06 18:43:30 -0400

    Added BandMember model

---
+++ MovieStore/Model/BandMember.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieStore.Model
+{
+    public class BandMember
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public DateTime dateOfBirth { get; set; }
+        public int age { get; set; }
+        public string gender { get; set; }
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+        public string skill { get; set; }
+
+    }
+}
