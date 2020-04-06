5b20065dabacc52e6a914b3794a95909f15603a9
Chris <jukes311@gmail.com>
2020-04-06 18:48:17 -0400

    Added BandMember and Tour controllers

---
+++ MovieStore/Controllers/BandMemberController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieStore.Controllers
+{
+    public class BandMemberController : Controller
+    {
+        // GET: BandMember
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
